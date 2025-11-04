77ee4008214ea621fd3d61712fe79ef6a819378e
teeramet <vasintherameth@gmail.com>
2025-11-04 08:50:45 -0700

    Updated cart test

--- test/pageobjects/cart.page.js
+++ test/pageobjects/cart.page.js
@@ -1,4 +1,4 @@
-import { $, browser } from '@wdio/globals'
+import { $, $$, browser } from '@wdio/globals'
 import Page from './page.js';
 import StorePage from './store.page.js';
 
@@ -7,12 +7,48 @@ class CartPage extends Page {
         return StorePage.cartBadge;
     }
 
-    get cartItem () {
-        return $('.cart_item');
+    get cartItemLabels () {
+        return $$('.cart_item_label');
     }
 
     get cartQuantity () {
         return $('.cart_quantity_label');
+    }
+
+    async assertBadge (amount) {
+        expect(this.cartBadge).toExist();
+        expect(this.cartBadge).toHaveText(expect.stringContaining(`${amount}`));
+    }
+
+    async assertCartItems () {
+        let products = StorePage.productsList;
+        let cartSize = await this.cartItemLabels.length;
+        
+        for (let i = 0; i < cartSize; i++) {
+            console.log('Start loop');
+            let itemName = await this.cartItemLabels[i].$('a').$('div').getText();
+            
+            switch (itemName) {
+                case products[0].name:
+                    await expect(this.cartItemLabels[i].$('.inventory_item_desc')).toHaveText(products[0].description);
+                    break;
+                case products[1].name:
+                    await expect(this.cartItemLabels[i].$('.inventory_item_desc')).toHaveText(products[1].description);
+                    break;
+                case products[2].name:
+                    await expect(this.cartItemLabels[i].$('.inventory_item_desc')).toHaveText(products[2].description);
+                    break;
+                case products[3].name:
+                    await expect(this.cartItemLabels[i].$('.inventory_item_desc')).toHaveText(products[3].description);
+                    break;
+                case products[4].name:
+                    await expect(this.cartItemLabels[i].$('.inventory_item_desc')).toHaveText(products[4].description);
+                    break;
+                case products[5].name:
+                    await expect(this.cartItemLabels[i].$('.inventory_item_desc')).toHaveText(products[5].description);
+                    break;
+            }
+        }
     }
 }
 
--- test/specs/cart-test.e2e.js
+++ test/specs/cart-test.e2e.js
@@ -1,24 +1,27 @@
-import LoginPage from '../pageobjects/login.page.js'
-import StorePage from '../pageobjects/store.page.js'
+import LoginPage from '../pageobjects/login.page.js';
+import StorePage from '../pageobjects/store.page.js';
+import CartPage from '../pageobjects/cart.page.js';
 
 async function initialize() {
     await LoginPage.open();
     await LoginPage.login('standard_user', 'secret_sauce');
 }
 
+let cartSize = 5;
+
 describe ('Cart features function correctly', () =>{
     it('should correctly add items to cart', async () => {
         initialize();
 
-        await StorePage.addToCart(3);
+        await StorePage.addToCart(cartSize);
         await StorePage.goToCart();
     })
-    // it('should create a badge on the cart icon', async () => {
-
-    // })
-    // it('should list item names with correct descriptions and prices', async () => {
-
-    // })
+    it('should create a badge on the cart icon', async () => {
+        await CartPage.assertBadge(cartSize);
+    })
+    it('should list item names with correct descriptions and prices', async () => {
+        await CartPage.assertCartItems();
+    })
     // it('should correctly remove items from the cart', async () => {
 
     // })
